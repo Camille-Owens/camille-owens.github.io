9e10b8382c6250fa7f99c61a3509120a0bc6c210
Camille-Owens <owe17013@byui.edu>
2019-02-08 20:26:44 -0700

    JS objects and loops

--- scripts/book.js
+++ scripts/book.js
@@ -1,9 +1,9 @@
 
-let Book = {
-	test: () => {
+function BookSearch() {
+	this.test = () => {
 		console.log('got this happening');
-	},
-	search: function() {
+	}
+	this.search = () => {
 		let searchInfo = document.querySelector('#book-search').value;
 		let search = new XMLHttpRequest();
 		let apiUrl = `https://openlibrary.org/search.json?q=${encodeURIComponent(searchInfo)}&page=1`;
@@ -41,44 +41,78 @@ let Book = {
 		    		// button that I want (by checking the id) before proceeding.
 		    		document.body.addEventListener('click', (e) => {
 		    			if (e.target && e.target.id == `${book.key}-button`) {
-		    				//in this case, 'this' is the Book object
+		    				console.log('in the wrong click event');
+		    				//in this case, 'this' is the BookSearch object
 		    				this.favorite(book);
 		    			}
 					});
 		    	}
 		    }
 		}
-	},
-	favorite: function(book) {
+	}
+	this.favorite = (book) => {
 		let favoriteBooks = localStorage.getItem('favoriteBooks') != null ? JSON.parse(localStorage.getItem('favoriteBooks')) : [];
 
-		favoriteBooks.push(book);
-		localStorage.setItem('favoriteBooks', JSON.stringify(favoriteBooks));
-		this.updateFavorites();
-	},
-	updateFavorites: function() {
+		let foundBook = favoriteBooks.filter(b => b.key === book.key);
+		console.log('foundBook', foundBook);
+		if (foundBook.length > 0) {
+			return;
+		} else {
+			favoriteBooks.push(book);
+			localStorage.setItem('favoriteBooks', JSON.stringify(favoriteBooks));
+			this.updateFavorites();
+		}
+	}
+	this.unFavorite = (book) => {
+		let favoriteBooks = localStorage.getItem('favoriteBooks') != null ? JSON.parse(localStorage.getItem('favoriteBooks')) : [];
+		console.log('favoriteBooks', favoriteBooks);
+		if (favoriteBooks.length > 0) {
+			let newFavoriteBooks = favoriteBooks.filter(b => b.key !== book.key);
+			console.log('newFavoriteBooks', newFavoriteBooks);
+			localStorage.setItem('favoriteBooks', JSON.stringify(newFavoriteBooks));
+			this.updateFavorites();
+		} else {
+			return;
+		}
+	}
+	this.updateFavorites = () => {
 		let favoriteBooks = JSON.parse(localStorage.getItem('favoriteBooks'));
+		console.log('favoriteBooks', favoriteBooks);
 
 		let favoriteBooksList = document.querySelector('#favorite-books-list');
 		favoriteBooksList.innerHTML = '';
 
-		if (!favoriteBooks) {
+		if (favoriteBooks.length === 0) {
 			favoriteBooksList.innerHTML = '<p>No Favorite Books</p>';
 		} else {
 			for (i = 0; i < favoriteBooks.length; i++) {
 				let book = favoriteBooks[i];
 	    		favoriteBooksList.innerHTML += `
 	    			<div>
-	    				<div id="${book.key}-book">${book.title} by ${book.author_name} </div>
+	    				<div id="${book.key}-favorite-book">${book.title} by ${book.author_name} </div>
 	    			</div>
 	    		`;
+	    		let unFavoriteButton = document.createElement('button');
+	    		unFavoriteButton.setAttribute('id', `${book.key}-unfavorite-button`);
+	    		unFavoriteButton.type = 'button';
+	    		unFavoriteButton.innerHTML = 'Remove';
+	    		document.getElementById(`${book.key}-favorite-book`).appendChild(unFavoriteButton);
+
+	    		document.body.addEventListener('click', (e) => {
+	    			if (e.target && e.target.id == `${book.key}-unfavorite-button`) {
+	    				console.log('in the right click event');
+	    				//in this case, 'this' is the BookSearch object
+	    				this.unFavorite(book);
+	    			}
+				});
 			}
 		}
 	}
 
 }
 
-Book.updateFavorites();
+let bookSearch = new BookSearch();
+bookSearch.updateFavorites();
 
 
 
