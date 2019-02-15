2bcff84bfa0930a942570f0ebd727ec032479602
Camille-Owens <owe17013@byui.edu>
2019-02-15 11:44:44 -0700

    update JSON Objects

--- scripts/book.js
+++ scripts/book.js
@@ -1,3 +1,51 @@
+function FavoriteBook(title, author, publishDate, rating = 0, apiId) {
+	this.title = title;
+	this.author = author;
+	this.publishDate = publishDate;
+	this.rating = rating;
+	this.apiId = apiId;
+
+	this.setRating = (rating) => {
+		if (!rating) {
+			return;
+		}
+		if (rating > 5 || rating < 0) {
+			window.alert('That is an invalid rating');
+			return;
+		}
+		this.rating = rating;
+		this.display();
+	}
+
+	this.display = () => {
+		let listItem = document.getElementById(`${this.apiId}-favorite-book`)
+		listItem.innerHTML = `${this.title} by ${this.author} | Rating: ${this.rating}/5 `;
+
+		let ratingEntry = document.createElement('input');
+		ratingEntry.setAttribute('id', `${this.apiId}-rating-input`);
+		ratingEntry.setAttribute('type', 'text');
+		ratingEntry.setAttribute('placeholder', 'Enter Rating from 0 to 5');
+		listItem.appendChild(ratingEntry);
+
+		let unFavoriteButton = document.createElement('button');
+		unFavoriteButton.setAttribute('id', `${this.apiId}-unfavorite-button`);
+		unFavoriteButton.type = 'button';
+		unFavoriteButton.innerHTML = 'Remove';
+		listItem.appendChild(unFavoriteButton);
+
+	}
+
+	this.formatForStorage = () => {
+		return {
+			title: this.title,
+			author: this.author,
+			publishDate: this.publishDate,
+			rating: this.rating,
+			apiId: this.apiId
+		}
+	}
+}
+
 
 function BookSearch() {
 	this.test = () => {
@@ -41,7 +89,6 @@ function BookSearch() {
 		    		// button that I want (by checking the id) before proceeding.
 		    		document.body.addEventListener('click', (e) => {
 		    			if (e.target && e.target.id == `${book.key}-button`) {
-		    				console.log('in the wrong click event');
 		    				//in this case, 'this' is the BookSearch object
 		    				this.favorite(book);
 		    			}
@@ -51,33 +98,35 @@ function BookSearch() {
 		}
 	}
 	this.favorite = (book) => {
+		//first we update the favorites
 		let favoriteBooks = localStorage.getItem('favoriteBooks') != null ? JSON.parse(localStorage.getItem('favoriteBooks')) : [];
 
-		let foundBook = favoriteBooks.filter(b => b.key === book.key);
-		console.log('foundBook', foundBook);
+		let foundBook = favoriteBooks.filter(b => b.apiId === book.key);
 		if (foundBook.length > 0) {
 			return;
 		} else {
-			favoriteBooks.push(book);
+			let favoriteBook = new FavoriteBook(book.title, book.author_name, book.first_publish_year, 0, book.key);
+			favoriteBooks.push(favoriteBook.formatForStorage());
 			localStorage.setItem('favoriteBooks', JSON.stringify(favoriteBooks));
-			this.updateFavorites();
+			this.updateFavoritesList();
 		}
+		//then we grey out the entry that has been moved to the favorites and remove the favorite button
+		let button = document.getElementById(`${book.key}-button`)
+		button.parentNode.removeChild(button);
+		let bookEntry = document.getElementById(`${book.key}-book`).classList.add('favorited-search-result');
 	}
 	this.unFavorite = (book) => {
 		let favoriteBooks = localStorage.getItem('favoriteBooks') != null ? JSON.parse(localStorage.getItem('favoriteBooks')) : [];
-		console.log('favoriteBooks', favoriteBooks);
 		if (favoriteBooks.length > 0) {
 			let newFavoriteBooks = favoriteBooks.filter(b => b.key !== book.key);
-			console.log('newFavoriteBooks', newFavoriteBooks);
 			localStorage.setItem('favoriteBooks', JSON.stringify(newFavoriteBooks));
-			this.updateFavorites();
+			this.updateFavoritesList();
 		} else {
 			return;
 		}
 	}
-	this.updateFavorites = () => {
+	this.updateFavoritesList = () => {
 		let favoriteBooks = JSON.parse(localStorage.getItem('favoriteBooks'));
-		console.log('favoriteBooks', favoriteBooks);
 
 		let favoriteBooksList = document.querySelector('#favorite-books-list');
 		favoriteBooksList.innerHTML = '';
@@ -86,25 +135,30 @@ function BookSearch() {
 			favoriteBooksList.innerHTML = '<p>No Favorite Books</p>';
 		} else {
 			for (i = 0; i < favoriteBooks.length; i++) {
-				let book = favoriteBooks[i];
-	    		favoriteBooksList.innerHTML += `
-	    			<div>
-	    				<div id="${book.key}-favorite-book">${book.title} by ${book.author_name} </div>
-	    			</div>
-	    		`;
-	    		let unFavoriteButton = document.createElement('button');
-	    		unFavoriteButton.setAttribute('id', `${book.key}-unfavorite-button`);
-	    		unFavoriteButton.type = 'button';
-	    		unFavoriteButton.innerHTML = 'Remove';
-	    		document.getElementById(`${book.key}-favorite-book`).appendChild(unFavoriteButton);
+				let bookEntry = favoriteBooks[i];
+				console.log('bookEntry', bookEntry);
+				//make each entry from localStorage a book object so it is easier to display and has the right information
+				let Book = new FavoriteBook(bookEntry.title, bookEntry.author, bookEntry.publishDate, bookEntry.rating, bookEntry.apiId);
+				favoriteBooksList.innerHTML += `
+					<div>
+						<div id="${Book.apiId}-favorite-book"></div>
+					</div>
+				`;
+				Book.display();
 
-	    		document.body.addEventListener('click', (e) => {
-	    			if (e.target && e.target.id == `${book.key}-unfavorite-button`) {
-	    				console.log('in the right click event');
-	    				//in this case, 'this' is the BookSearch object
-	    				this.unFavorite(book);
-	    			}
+				// we add the events out here because we only want to do them once
+				document.body.addEventListener('click', (e) => {
+					if (e.target && e.target.id == `${Book.apiId}-unfavorite-button`) {
+						console.log('in the right click event');
+						//in this case, 'this' is the BookSearch object
+						this.unFavorite(bookEntry);
+					}
 				});
+				document.body.addEventListener('change', (e) => {
+					if (e.target && e.target.id == `${Book.apiId}-rating-input`) {
+						Book.setRating(e.target.value);
+					}
+				})
 			}
 		}
 	}
@@ -112,7 +166,7 @@ function BookSearch() {
 }
 
 let bookSearch = new BookSearch();
-bookSearch.updateFavorites();
+bookSearch.updateFavoritesList();
 
 
 
