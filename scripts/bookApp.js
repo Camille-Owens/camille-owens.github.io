e5e7e29cda0e49ca2dcc09ef0ca2be4a61037aec
Camille-Owens <owe17013@byui.edu>
2019-03-01 16:12:59 -0700

    adding the stars

--- scripts/bookApp.js
+++ scripts/bookApp.js
@@ -7,8 +7,11 @@ function FavoriteBook(title, author, publishDate, rating = 0, apiId, haveRead = 
 	this.haveRead = haveRead;
 
 	this.setRating = (rating) => {
-		if (!rating) {
-			return;
+		// if (!rating) {
+		// 	return;
+		// }
+		if (!Number.isInteger(rating)) {
+			window.alert('You must enter a whole number');
 		}
 		if (rating > 5 || rating < 0) {
 			window.alert('That is an invalid rating');
@@ -20,7 +23,13 @@ function FavoriteBook(title, author, publishDate, rating = 0, apiId, haveRead = 
 
 	this.display = () => {
 		let listItem = document.getElementById(`${this.apiId}-favorite-book`)
-		listItem.innerHTML = `<div><span class="favorite-book-title">${this.title} by ${this.author}<span class="badge">${this.rating}/5</span></span></div>`;
+		listItem.innerHTML = `<div><span class="favorite-book-title">${this.title} by ${this.author}</span></div>`;
+
+		let stars = document.createElement('div');
+		// <span id="${this.apiId}-star-rating" class="rating"></span>
+		stars.setAttribute('id', `${this.apiId}-star-rating`);
+		stars.classList.add('rating');
+		listItem.appendChild(stars);
 
 		let ratingEntry = document.createElement('input');
 		ratingEntry.setAttribute('id', `${this.apiId}-rating-input`);
@@ -46,6 +55,83 @@ function FavoriteBook(title, author, publishDate, rating = 0, apiId, haveRead = 
 		}
 		listItem.appendChild(haveReadButton);
 
+		this.drawRating();
+
+	}
+
+	this.drawRating = () => {
+		for (let i = 1; i <= 5; i++) {
+			this.readyCanvas(i);
+		}
+
+		let stars = document.getElementsByClassName('star-canvas');
+		for (let i = 0; i < stars.length; i++) {
+			this.drawStar(stars[i]);
+		}
+	}
+
+	this.readyCanvas = (index) => {
+		let ratingPlacement = document.getElementById(`${this.apiId}-star-rating`);
+
+		let star = document.createElement('canvas');
+		star.setAttribute('width', '30');
+		star.setAttribute('height', '30');
+		// I was having some trouble getting the stars to be selected with the apiId as an index, so I store the rating and
+		// which star number it is in each star as data- attributes
+		star.setAttribute('data-rating', this.rating);
+		star.setAttribute('data-index', index);
+		star.classList.add(`star-canvas`);
+
+		ratingPlacement.appendChild(star);
+	}
+
+	this.drawStar = (star) => {
+		let rating = star.getAttribute('data-rating');
+		let index = star.getAttribute('data-index');
+
+		//set if we want the star filled or empty
+		let fill = (index <= rating) && rating != 0;
+
+		// get the 2d context of the canvas element
+		let ctx = star.getContext("2d");
+
+		//set the colors of the fill and stroke (the lines that you make when drawing)
+		ctx.fillStyle = "#8B7752";
+	    ctx.strokeStyle = "#8B7752";
+
+	    // Ok, so I knew I wanted to draw a star, so I borrowed some code and tweaked it a little to get what I wanted
+		// I'll explain here so you can understand that I understand what's going on
+
+	    //So, in order to get the star's ponts even, I draw them inside a circle. The points will hit at a radius of 10
+		let radius = 10; // this is the length of the star's point
+		// move so the star can be centered
+		ctx.translate(10, 17);
+
+		// When I just did the code below with the 5 points, the star was tilted wonky, so I 
+		// rotated it at the start so it would be straight.
+		// Each of the star's points take up 1/5 of the circle, I rotated it half of a star's point
+		ctx.rotate((Math.PI * 1 / 10));
+
+		// do this 5 times to make 5 points
+		for (let i = 0; i < 5; i++) {
+			// this is the code that I borrowed in order to make a star
+			// this is making one point at a time
+		    ctx.lineTo(0, radius);
+		    ctx.translate(0, radius);
+		    ctx.rotate((Math.PI * 2 / 10));
+		    ctx.lineTo(0, -radius);
+		    ctx.translate(0, -radius);
+		    ctx.rotate(-(Math.PI * 6 / 10));
+		}
+		// connect to starting point
+		ctx.lineTo(0, radius);
+		ctx.closePath();
+
+		if (fill) {
+			ctx.fill();
+		} else {
+			ctx.stroke();
+		}
 	}
 
 	this.formatForStorage = () => {
@@ -128,7 +214,7 @@ function BookSearch() {
 		if (foundBook.length > 0) {
 			return;
 		} else {
-			let favoriteBook = new FavoriteBook(book.title, book.author_name, book.first_publish_year, 0, book.key, false);
+			let favoriteBook = new FavoriteBook(book.title, book.author_name, book.first_publish_year, 0, book.key.replace('/','').replace('\/', ''), false);
 			favoriteBooks.push(favoriteBook.formatForStorage());
 			localStorage.setItem('favoriteBooks', JSON.stringify(favoriteBooks));
 			// this.updateFavoritesList();
@@ -141,10 +227,8 @@ function BookSearch() {
 		document.querySelector('.badge').classList.remove('hidden');
 	}
 	this.unFavorite = (book) => {
-		console.log('unfavorite');
 		let favoriteBooks = localStorage.getItem('favoriteBooks') != null ? JSON.parse(localStorage.getItem('favoriteBooks')) : [];
 		if (favoriteBooks.length > 0) {
-			console.log('bookslength');
 			let newFavoriteBooks = favoriteBooks.filter(b => b.apiId !== book.apiId);
 			localStorage.setItem('favoriteBooks', JSON.stringify(newFavoriteBooks));
 			this.updateFavoritesList();
@@ -167,7 +251,6 @@ function BookSearch() {
 				return b;
 
 			});
-			console.log('updatedFavoriteBooks', updatedFavoriteBooks);
 			// updatedFavoriteBooks.push(Book.formatForStorage());
 			localStorage.setItem('favoriteBooks', JSON.stringify(updatedFavoriteBooks));
 			this.updateFavoritesList();
@@ -184,7 +267,6 @@ function BookSearch() {
 		} else {
 			for (i = 0; i < favoriteBooks.length; i++) {
 				let bookEntry = favoriteBooks[i];
-				console.log('bookEntry', bookEntry);
 				//make each entry from localStorage a book object so it is easier to display and has the right information
 				let Book = new FavoriteBook(bookEntry.title, bookEntry.author, bookEntry.publishDate, bookEntry.rating, bookEntry.apiId, bookEntry.haveRead);
 				favoriteBooksList.innerHTML += `
@@ -195,14 +277,13 @@ function BookSearch() {
 				// we add the events out here because we only want to do them once
 				document.body.addEventListener('click', (e) => {
 					if (e.target && e.target.id == `${Book.apiId}-unfavorite-button`) {
-						console.log('in the right click event');
 						//in this case, 'this' is the BookSearch object
 						this.unFavorite(Book);
 					}
 				});
 				document.body.addEventListener('change', (e) => {
 					if (e.target && e.target.id == `${Book.apiId}-rating-input`) {
-						Book.setRating(e.target.value);
+						Book.setRating(parseInt(e.target.value));
 						this.updateFavorite(Book);
 					}
 				})
@@ -217,6 +298,8 @@ function BookSearch() {
 	}
 
 }
+
+
 
 let bookSearch = new BookSearch();
 // bookSearch.updateFavoritesList();
